0e5532a9396efddb14a3a5109a4690caff8b900e
Michael Farr <mfarr@sm-energy.com>
2019-08-17 12:35:25 -0500

    update after dsu update

--- tc_workflow.R
+++ tc_workflow.R
@@ -48,14 +48,12 @@ for(i in 1:length(file.names)){
   names(tmp)<-gsub("[()]", "", names(tmp))
   names(tmp)<-gsub("/","_",  names(tmp))
   names(tmp)<-gsub("\\%", "", names(tmp))
+  names(tmp)<-tolower(names(tmp))
   assign(file.names[i], tmp)
   rm(tmp)
   
 }
 
-res1 <- clean_dt(resource)
-names(resource)
-gsub("\\.", "_", names(resource))
 
 ##  for loop to remove all attribute (except for datetime columes)
 for(i in seq_along(file.names)){
@@ -84,34 +82,15 @@ for(i in seq_along(file.names)){
 ##  build resource model table from dsu spreasheet=============================
 
 
-##  lateral length bins for tbl cleanup
-lat_lngth <- c("7000", "10000", "13000", "16000")
-##  all the unique scenario names
-scenario_names <- unique(sort(resource$Scenario))
-
-#i<-1
-##  for loop to transpose data tbl
-dt_res <- data.frame()
-for(i in seq_along(lat_lngth)){
-  
-  indx <- grepl(lat_lngth[i], colnames(resource));
-  tmp_res <- resource[1:3]
-  tmp_res <- cbind(tmp_res, resource[indx]);
-  tmp_res$eff_lat <- lat_lngth[i];
-  names(tmp_res) <- c("scenario", "tc_sub_region", "corpcall_dec", 
-                      "well_count", "spacing", "eff_lat");
-  dt_res <- rbind(dt_res, tmp_res);
-  
-}
-
 ## clean data in friendly format
-dt_res <- clean_dt(dt_res)
+resource <- clean_dt(resource)
 
-res_spacing_names <- unique(dt_res$spacing)
-res_sub_names <- unique(dt_res$tc_sub_region)
-res_zone_lower <- res_spacing_names[grepl("LOWER", res_spacing_names)]
-res_zone_int <- res_spacing_names[grepl("INT", res_spacing_names)]
+
+res_spacing_names <- unique(resource$spacing_name)
+res_sub_names <- unique(resource$tc_sub_region)
 res_h2s <- res_sub_names[grepl("H2S", res_sub_names)]
+res_zone_int <- res_spacing_names[grepl("INT", res_spacing_names)]
+res_zone_lower <- res_spacing_names[grepl("LOWER", res_spacing_names)]
 res_spacing_cat <- c("625", "900", "1250")
 res_h_tc_region <- c("H_EAST", "H_WEST")
 res_mod_awest <- c("A2W", "A4W", "A6W")
@@ -122,19 +101,18 @@ res_distric_east <- c("A10E", "A12", "A2", "A20", "A40", "A4W", "A6E", "C2")
 res_distric_south <- c("B10", "B20", "B3", "B40", "C0", "C20", "C7", "D0")
 
 ##  build resource model table
-res_mod <- dt_res %>% ##  fix h2s column
+res_mod <- resource %>% ##  fix h2s column
   filter(tc_sub_region %in% res_h2s &
-           !spacing %in% res_zone_int) %>%
+           !spacing_name %in% res_zone_int) %>%
   mutate(h2s = "H2S") %>%
-  bind_rows(dt_res %>%##  fix noh2s column
+  bind_rows(resource %>%##  fix noh2s column
               filter(!tc_sub_region %in% res_h2s) %>%
               mutate(h2s = "NOH2S")) %>%
-  bind_rows(dt_res %>%
-              filter(spacing %in% res_zone_int) %>%
-              mutate(h2s = "NOH2S")) %>%
-  mutate(reservoir = if_else(word(spacing, 2) %in% res_spacing_cat, "UEF", 
-                        word(spacing, 2)), 
-         spacing = word(spacing, 1),
+  bind_rows(resource %>% ##  fix h2s column
+    filter(spacing_name %in% res_zone_int ) %>%
+  mutate(h2s = "NOH2S")) %>%
+  mutate(reservoir = if_else(spacing_name %in% res_zone_int, "INT", lower_upper),
+         spacing = stick_map,
          zone = case_when(reservoir == "LOWER" ~ "LEF", 
                           reservoir == "INT" ~ "IEF", 
                           TRUE ~ "UEF"),
@@ -151,14 +129,11 @@ res_mod <- dt_res %>% ##  fix h2s column
          cc_dec = case_when(corpcall_dec %in% res_cc_dec_no_zone ~ corpcall_dec,
                             zone %in% "IEF" ~ paste0(corpcall_dec, "_", "LEF"),
                             TRUE ~ paste0(corpcall_dec, "_", zone)), 
-         well_count = as.numeric(well_count)) %>% 
-  filter(well_count > 0) %>%
-  ##  duplicate rows based on well_count
-  slice(rep(seq_len(n()), well_count)) %>%
-  mutate(well_count = 1, 
+         well_count = 1,
+         eff_lat = lateral_bin,
          tc_name = paste0(tc_region, "_", tc_sub, "_", zone, "_", h2s, "_", 
                           cc_dec, "_",eff_lat, "_", spacing)) %>%
-  select(scenario, tc_name, reservoir, zone, tc_region, tc_sub, h2s, 
+  select(scenario, scenario_uid ,tc_name, reservoir, zone, tc_region, tc_sub, h2s, 
          cc_dec, eff_lat, spacing, well_count) %>%
   mutate(tc_lookup = case_when(tc_region == "A_WEST" | tc_region == "A_EAST" ~ "A", 
                                tc_region == "SAA" | tc_region == "H_EAST" | tc_region == "H_WEST" |
@@ -200,7 +175,7 @@ res_mod$tc_name <- str_replace(res_mod$tc_name, "A4E", "A6E")
 
 
 res_mod %>%
-  group_by(scenario, zone) %>%
+  group_by(scenario) %>%
   summarise(sum(well_count))
 
 ##  when you use group_by it changes the class of the data table to a tbl 
@@ -217,7 +192,7 @@ res_mod %>%
 
 ##  build briscoe lookup tbl===================================================
 
-br_lease_names <- unique(tc_br$LEASE)
+br_lease_names <- unique(tc_br$lease)
 br_saa <- br_lease_names[grepl("SA A", br_lease_names)]
 br_h_east <- br_lease_names[grepl("H-EAST", br_lease_names)& !grepl("SA A", br_lease_names)]
 br_f_west <- br_lease_names[grepl("F-WEST", br_lease_names)]
@@ -230,9 +205,9 @@ br_d <- br_lease_names[grepl("D0", br_lease_names)]
 
 
 briscoe_lease_names <- tc_br %>%
-  filter(LEASE %in% br_areab_6) %>%
-  distinct(LEASE) %>%
-  mutate(L1 = gsub("-", " ", LEASE), 
+  filter(lease %in% br_areab_6) %>%
+  distinct(lease) %>%
+  mutate(L1 = gsub("-", " ", lease), 
          L2 = gsub("/", " ", L1),
          tc_region = substr(word(L2, 2), start = 1, stop = 1),
          tc_sub = word(L2, 2),
@@ -242,9 +217,9 @@ briscoe_lease_names <- tc_br %>%
          h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
          cc_dec = paste0("AREAB_6","_",word(L2, 3))) %>%
   bind_rows(tc_br %>%
-              filter(LEASE %in% br_cc_3.7) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(lease %in% br_cc_3.7) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = substr(word(L2, 2), start = 1, stop = 1),
                      tc_sub = word(L2, 2),
@@ -254,9 +229,9 @@ briscoe_lease_names <- tc_br %>%
                      h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                      cc_dec = paste0(word(L2, -2),"_",word(L2, 3)))) %>%
   bind_rows(tc_br %>% 
-              filter(LEASE %in% br_bd & !LEASE %in% br_cc_3.7 & !LEASE %in% br_areab_6) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(lease %in% br_bd & !lease %in% br_cc_3.7 & !lease %in% br_areab_6) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = substr(word(L2, 2), start = 1, stop = 1),
                      tc_sub = word(L2, 2),
@@ -266,9 +241,9 @@ briscoe_lease_names <- tc_br %>%
                      h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                      cc_dec = paste0(word(L2, -2),"_",word(L2, 3)))) %>%
   bind_rows(tc_br %>% 
-              filter(LEASE %in% br_saa) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(lease %in% br_saa) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = "SAA",
                      tc_sub = paste0(word(L2,4),"_",word(L2,5)),
@@ -278,9 +253,9 @@ briscoe_lease_names <- tc_br %>%
                      h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                      cc_dec = word(L2, -2))) %>%
   bind_rows(tc_br %>% 
-              filter(LEASE %in% br_h_east) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(lease %in% br_h_east) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = paste0(word(L2, 2),"_",word(L2,3)),
                      tc_sub = tc_region,
@@ -290,9 +265,9 @@ briscoe_lease_names <- tc_br %>%
                      h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                      cc_dec = word(L2, -2))) %>%
   bind_rows(tc_br %>% 
-              filter(LEASE %in% br_f_west) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(lease %in% br_f_west) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = "F_WEST",
                      tc_sub = word(L2, 2),
@@ -302,9 +277,9 @@ briscoe_lease_names <- tc_br %>%
                      h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                      cc_dec = word(L2, -3))) %>%
   bind_rows(tc_br %>% 
-              filter(LEASE %in% br_h_west) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(lease %in% br_h_west) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = paste0(word(L2, 2),"_",word(L2,3)),
                      tc_sub = tc_region,
@@ -314,11 +289,11 @@ briscoe_lease_names <- tc_br %>%
                      h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                      cc_dec = paste0(word(L2, -2)))) %>%
   bind_rows(tc_br %>% 
-            filter(!LEASE %in% br_f_west & !LEASE %in% br_h_east & 
-                     !LEASE %in% br_saa & !LEASE %in% br_h_west & 
-                     !LEASE %in% br_bd & !LEASE %in% br_cc_3.7 & !LEASE %in% br_areab_6) %>%
-            distinct(LEASE) %>%
-            mutate(L1 = gsub("-", " ", LEASE), 
+            filter(!lease %in% br_f_west & !lease %in% br_h_east & 
+                     !lease %in% br_saa & !lease %in% br_h_west & 
+                     !lease %in% br_bd & !lease %in% br_cc_3.7 & !lease %in% br_areab_6) %>%
+            distinct(lease) %>%
+            mutate(L1 = gsub("-", " ", lease), 
                    L2 = gsub("/", " ", L1),
                    tc_region = substr(word(L2, 2), start = 1, stop = 1),
                    tc_sub = word(L2, 2),
@@ -327,22 +302,22 @@ briscoe_lease_names <- tc_br %>%
                    spacing = word(L2,4), 
                    h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                    cc_dec = word(L2, -2))) %>% 
-  select(LEASE, tc_region, tc_sub, eff_lat, zone, spacing, h2s, cc_dec) %>%
+  select(lease, tc_region, tc_sub, eff_lat, zone, spacing, h2s, cc_dec) %>%
   mutate(TEAM = "BRISCOE")
 
 
 
 ##  build galvan lookup tbl====================================================
 
-lease_gr <- unique(tc_gr$LEASE)
+lease_gr <- unique(tc_gr$lease)
 a_west <- lease_gr[grepl("W", lease_gr)]
 tc_match <- c("A12E", "A10E", "A8E", "A6E", "A4E")
 a_east <- lease_gr[grepl(paste(tc_match, collapse = "|"), lease_gr)]
 
 galvan_tc_name <- tc_gr %>% 
-  filter(LEASE %in% a_west) %>%
-  distinct(LEASE) %>%
-  mutate(L1 = gsub("-", " ", LEASE), 
+  filter(lease %in% a_west) %>%
+  distinct(lease) %>%
+  mutate(L1 = gsub("-", " ", lease), 
          L2 = gsub("/", " ", L1),
          tc_region = "A_WEST",
          tc_sub = word(L2, 2), 
@@ -353,9 +328,9 @@ galvan_tc_name <- tc_gr %>%
          cc_dec = if_else(word(L2, 3) == "IEF", word(L2, -2),
                           paste0(word(L2, -2), "_", word(L2, 3)))) %>%
   bind_rows(tc_gr %>% 
-              filter(LEASE %in% a_east) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(lease %in% a_east) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = "A_EAST",
                      tc_sub = word(L2, 2), 
@@ -366,9 +341,9 @@ galvan_tc_name <- tc_gr %>%
                      cc_dec = if_else(word(L2, 3) == "IEF", word(L2, -2),
                                       paste0(word(L2, -2), "_", word(L2, 3))))) %>%
   bind_rows(tc_gr %>%
-              filter(!LEASE %in% a_east & !LEASE %in% a_west) %>%
-              distinct(LEASE) %>%
-              mutate(L1 = gsub("-", " ", LEASE), 
+              filter(!lease %in% a_east & !lease %in% a_west) %>%
+              distinct(lease) %>%
+              mutate(L1 = gsub("-", " ", lease), 
                      L2 = gsub("/", " ", L1),
                      tc_region = substr(word(L2, 2), start = 1, stop = 1),
                      tc_sub = word(L2, 2), 
@@ -378,7 +353,7 @@ galvan_tc_name <- tc_gr %>%
                      h2s = if_else(grepl("H2S", L2), "H2S", "NOH2S"),
                      cc_dec = if_else(word(L2, 3) == "IEF", word(L2, -2),
                                       paste0(word(L2, -2), "_", word(L2, 4))))) %>%
-  select(LEASE, tc_region, tc_sub, eff_lat, zone, spacing, h2s, cc_dec) %>%
+  select(lease, tc_region, tc_sub, eff_lat, zone, spacing, h2s, cc_dec) %>%
   mutate(TEAM = "GALVAN")
 
 
@@ -396,13 +371,13 @@ res_mod$id <- comb_tc_names[match(res_mod$tc_name, comb_tc_names$tc_name),11]
 
 tc_comb <- bind_rows(tc_br, tc_gr)
 
-prop <- unique(tc_comb$PROPNUM)
+prop <- unique(tc_comb$propnum)
 
 #i<-1
 tc_lookup <- data.frame()
 for(i in seq_along(prop)){
-  tc_tmp <- subset(tc_comb, PROPNUM == prop[i]);
-  LEASE <- tc_tmp[1,16];
+  tc_tmp <- subset(tc_comb, propnum == prop[i]);
+  lease <- tc_tmp[1,16];
   BU_Q1 <- tc_tmp[2,2]
   BU_T1 <- tc_tmp[2,5];
   BU_D1 <- tc_tmp[2,8];
@@ -430,7 +405,7 @@ for(i in seq_along(prop)){
   }
 
 
-  tc_tmp2 <- data.frame(LEASE, BU_Q1, BU_T1, BU_D1, EXP_Q1, EXP_T1, EXP_D1, 
+  tc_tmp2 <- data.frame(lease, BU_Q1, BU_T1, BU_D1, EXP_Q1, EXP_T1, EXP_D1, 
                         HYP_T1, HYP_B1, HYP_D1, HYP_Q4, HYP_DMIN, 
                         YIELD_Q1, YIELD_Q2, YIELD_T1, YIELD_Q3, YIELD_Q4)
   
@@ -442,7 +417,7 @@ for(i in seq_along(prop)){
 
 tc_lookup <- left_join(tc_lookup, 
                        comb_tc_names %>%
-                         select(LEASE, id, tc_name), by = "LEASE") %>%
+                         select(lease, id, tc_name), by = "lease") %>%
   na.omit() %>%
   select(tc_name, id, BU_Q1, BU_T1, BU_D1, EXP_Q1, EXP_T1, EXP_D1,
          HYP_T1, HYP_B1, HYP_D1, HYP_Q4, HYP_DMIN, YIELD_Q1, YIELD_Q2,
@@ -450,11 +425,13 @@ tc_lookup <- left_join(tc_lookup,
   
 
 #na_count <- sapply(tc_lookup, function(y) sum(is.na(y)))
+#write.csv(tc_lookup, file = "lookup.csv")
+#write.csv(galvan_tc_name, file = "gr_tc_name.csv")
 ##  open connect to access==========================================
 
 ##doc property
-AccessFilePath <- "C:/Users/mfarr/Documents/Aries db/etc_resource_model.accdb"
-#AccessFilePath <- "N:/Dept/Prod/Aries/Db/Houston/2019 Projects/ETC Renegotiation WIG/etc_resource_model.accdb"
+#AccessFilePath <- "C:/Users/mfarr/Documents/Aries db/etc_resource_model.accdb"
+AccessFilePath <- "N:/Dept/Prod/Aries/Db/Houston/2019 Projects/ETC Renegotiation WIG/etc_resource_model.accdb"
 driver <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)}"
 dLocation <- AccessFilePath
 ch <- odbcDriverConnect(paste(driver,';DBQ=',dLocation))
@@ -490,37 +467,40 @@ res_mod <- res_mod %>%
          spacing = as.numeric(spacing))
 
 ##  ac_property key  ##
-##  corp11 = H2S | corp12 = lookup id | corp13 = scenario | corp14 = tc_name | corp15 = tc_zone
+##  corp9 spacing_uid | corp11 = H2S | corp12 = lookup id | corp13 = scenario | corp14 = tc_name | 
+##  corp15 = tc_zone
 ##  wtr_loe1 = water disposal cost 1 | wtr_loe2 = water disposal cost 2 | intan_cmpl = ultrafab cost
 ##  intan_drl = h2s gas trans cost
 
-ac_p$LEASE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 22]
+#dt <- data.frame(nm = names(res_mod))
+
+ac_p$LEASE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 23]
+ac_p$CORP9 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 2]
 ac_p$CORP10 <- "UPSIDE"
-ac_p$CORP11 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 7]
-ac_p$CORP12 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),23]
+ac_p$CORP11 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 8]
+ac_p$CORP12 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),24]
 ac_p$CORP13 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),1]
-ac_p$CORP14 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),2]
-ac_p$CORP15 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),13]
-ac_p$TEAM <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 14]
-ac_p$EFF_LAT <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 9]
-ac_p$ZONE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 4]
-ac_p$WELL_SPACING_PLAN_VIEW <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),10]
+ac_p$CORP14 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),3]
+ac_p$CORP15 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),14]
+ac_p$TEAM <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 15]
+ac_p$EFF_LAT <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 10]
+ac_p$ZONE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 5]
+ac_p$WELL_SPACING_PLAN_VIEW <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),11]
 ac_p$TYPE_CURVE_REGION <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 5]
-ac_p$TYPE_CURVE_SUBREGION <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 6]
-ac_p$CORPCALL_DEC <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),8]
-ac_p$FRAC_DESIGN <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),15]
-ac_p$LOE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),20]
-ac_p$WOE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),19]
-ac_p$WTR_LOE1 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),18]
-ac_p$WTR_LOE2 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),18]
-ac_p$DISTRICT <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),17]
-ac_p$INTAN_CMPL <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),16]
-ac_p$INTAN_DRL <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),21]
+ac_p$TYPE_CURVE_SUBREGION <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM), 7]
+ac_p$CORPCALL_DEC <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),9]
+ac_p$FRAC_DESIGN <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),16]
+ac_p$LOE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),21]
+ac_p$WOE <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),20]
+ac_p$WTR_LOE1 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),19]
+ac_p$WTR_LOE2 <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),19]
+ac_p$DISTRICT <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),18]
+ac_p$INTAN_CMPL <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),17]
+ac_p$INTAN_DRL <- res_mod[match(ac_p$PROPNUM, res_mod$PROPNUM),22]
 
 AC_PROPERTY <- bind_rows(AC_PROPERTY %>%
                            filter(CORP10 != "UPSIDE"), 
                          ac_p)
-
 
 AC_PROPERTY %>%
   filter(PROPNUM == "U8AALCSBBH") %>%
@@ -542,7 +522,7 @@ close(ch)
 
 ##  end of script===================================================
 
-#write.csv(dt_res, file = "dt_res.csv")
+#write.csv(resource, file = "resource.csv")
 #write.csv(dtx, file = "dtx.csv")
 #write.csv(briscoe_lease_names, file = "br_tc.csv")
 write.csv(tc_lookup, file = "tc_lookup.csv")
